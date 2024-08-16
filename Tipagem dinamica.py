9682c581e5b6afbdb876bc61f66a5b603fd8fa50
João Victor Mendes <joao176069@gmail.com>
2024-08-16 18:14:14 +0000

    Tipos primitivos.

---
+++ Tipagem dinamica.py
@@ -0,0 +1,15 @@
+import os 
+
+os.system (" cls || clear ")
+
+nome : str # Semelhante a cadeia 
+idade : int # Inteiro  
+altura : float # Real 
+
+nome = "Joao "
+idade = 18 
+altura = 1.65
+
+print (f" Nome : { nome }")
+print (f" Idade : {idade }")
+print ( f" altura : { altura }")
---
+++ Tipos Primitivos./Tipos estatica.py
@@ -0,0 +1,16 @@
+import os
+os.system (" cls || clear ") # limpa o terminal 
+
+# Tipagem estática.
+nome : str # Semelhante a cadeia 
+idade : int # Inteiro  
+altura : float # Real 
+
+nome = "Joao "
+idade = 18 
+altura = 1.65
+
+print ( nome )
+print ( idade )
+print ( altura )
+
