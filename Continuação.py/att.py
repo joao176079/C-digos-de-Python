a06e991dba49df0cb5b6c961141ba7821f40aa0a
João Victor Mendes <joao176069@gmail.com>
2024-08-26 19:53:26 +0000

    kk

---
+++ Continuação.py/att.py
@@ -0,0 +1,10 @@
+import time 
+import os 
+os.system("clls| clear")
+
+numero = int (input ("Digite um número : "))
+
+for numero in range (numero ,0,-1) :
+    print (numero)
+    time.sleep(1)
+
--- atividade 1.py
+++ atividade 1.py
@@ -1,17 +1,24 @@
 import os 
 os.system("clls| clear")
 soma = 0
-resultado = 0
+media = 0
 
 print("Coloque as suas notas , e ao final ira vir a média. ")
 
 
 
-for i in range (4): 
+for i in range (3): 
     
     numero = int ( input (f" Digite a sua primeiro nota  {i+1} : "))
     
     soma = soma + numero
-    resultado = (soma + numero) /  4
+    media = (soma + media) /  4
     
-print (f"Soma  : {resultado} ")
+print (f"Nota : {media:.1f} ")
+
+if media >7 :
+    print ("O aluno está aprovado. ")
+elif media >= 4 :
+    print ("O aluno está de recuperação. ")
+else :
+    print ("O aluno está reprovado. ")
